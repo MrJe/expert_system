e0148cfd43046a6f264ea34d1d121f94d7190198
Gaetan POBLON <gpoblon@e1r2p20.42.fr>
2019-09-11 16:56:15 +0200

    wip file handling

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,3 +1,18 @@
+// fn main() {
+// 	println!("I'm using the library: {:?}", lib::modules_checker().unwrap());
+// }
+
 fn main() {
-	println!("I'm using the library: {:?}", lib::modules_checker().unwrap());
+	let args: Vec<String> = std::env::args().collect();
+	if args.len() == 2 {
+		println!("{}", &args[1]);
+		match lib::get_file_contents(&args[1]) {
+			Ok((_, contents))	=> println!("FILE CONTENTS : {}", contents),
+			Err(err)			=> return println!("{}", err)
+		}
+		let _output_file = lib::output_to_file("RESULT.txt");
+	}
+	else {
+		println!("Usage: only 2 parameters !");
+	}
 }
--- src/lib.rs
+++ src/lib.rs
@@ -9,18 +9,7 @@ use std::fs::File;
 use std::io::Error;
 use std::io::prelude::*;
 
-pub fn modules_checker() -> Result<bool, Error> { // just to try, don't use
-	let input_file = file::open("example_input.txt")?;
-	file::read(false)?;
-	file::parser()?;
-	file::print(&input_file)?;
-	let output_file = file::create("output_test.txt")?;
-	file::write(&output_file, "Here is our first file created in Rust !")?;
-	Ok(true)
-}
-
-
-pub fn get_file(fname: &str) -> Result<(File, String), Error> {
+pub fn get_file_contents(fname: &str) -> Result<(File, String), Error> {
 	let mut f = File::open(fname)?;
 	let mut contents = String::new();
 	f.read_to_string(&mut contents)?;
