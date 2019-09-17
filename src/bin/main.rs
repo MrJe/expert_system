c8a99fe886aa10153e9002a9ff45e8f6f3bb1b3e
jmichaud <jmichaud@student.42.fr>
2019-09-17 14:30:34 +0200

    main with correct open()

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,19 +1,31 @@
+use std::env;
+use std::fs::File;
+
+fn expert_system(file: File) {
+	println!("expert_system() launched !");
+	// let mut facts = lib::parser::Facts::new();
+	let solver = lib::file::parser(&file).unwrap();
+	solver.facts.print('A');
+	solver.facts.print('B');
+	solver.facts.print('C');
+	solver.facts.print('V');
+	solver.facts.print('X');
+	solver.rules_printer();
+	// let _res = lib::file::output_result("RESULT.txt", &(solver.facts));
+
+}
+
 fn main() {
-	let args: Vec<String> = std::env::args().collect();
-	if args.len() == 2 {
-		println!("{}", &args[1]);
-		let f = std::fs::File::open(&args[1]).unwrap();
-		// let mut facts = lib::parser::Facts::new();
-		let solver = lib::file::parser(&f).unwrap();
-		solver.facts.print('A');
-		solver.facts.print('B');
-		solver.facts.print('C');
-		solver.facts.print('V');
-		solver.facts.print('X');
-		solver.rules_printer();
-		// let _res = lib::file::output_result("RESULT.txt", &(solver.facts));
-	}
-	else {
-		println!("Usage: only 2 parameters !");
-	}
+	let args: Vec<String> = env::args().collect();
+
+    if args.len() != 2 {
+        println!("usage: ./expert_system input_file");
+    }
+    else {
+        let filename = &args[1];
+        match File::open(filename) {
+            Ok(file)    => expert_system(file),
+            Err(error)  => println!("open: {}: {}", filename, error),
+        };
+    }
 }
