93289a7db673fe1fd53a2cf09aa55aa710fa09bd
jmichaud <jmichaud@student.42.fr>
2019-09-27 13:31:28 +0200

    add lot of tests

--- src/tests.rs
+++ src/tests.rs
@@ -14,13 +14,13 @@ fn get_operand(c: char) -> Operand {
     }
 }
 
-fn tokenise_str(arg: &str) -> Vec<Token> {
+fn tokenise_str<'rule>(arg: &str, facts: &'rule Vec<Fact>) -> Vec<Token<'rule>> {
     let mut ret: Vec<Token> = Vec::new();
 
     for c in arg.chars() {
         match c {
             ' ' | '\t'  => continue,
-            'A'..='Z'   => ret.push(Token::new(None, Some(&Fact::new(c)))),
+            'A'..='Z'   => ret.push(Token::new(None, Some(&facts[c as usize - 65]))),
             _           => ret.push(Token::new(Some(get_operand(c)), None)),
         }
     }
@@ -40,10 +40,59 @@ fn print_tokens(tokens: &Vec<Token>) {
 }
 
 fn cmp_tokens(exp: Vec<Token>, res: Vec<Token>) -> bool {
-    if exp.len() != res.len() {
+    let mut i = exp.len();
+    if i != res.len() {
         return false
     }
+    while i > 0 {
+        i -= 1;
+        if exp[i].cmp_tok(&res[i]) == false {
+            return false
+        }
+    }
     true
+}
+
+fn run_test(expr: &str, rslt: &str, ass: bool) -> Result<(), Error> {
+    let facts = vec![
+        Fact::new('A'),
+        Fact::new('B'),
+        Fact::new('C'),
+        Fact::new('D'),
+        Fact::new('E'),
+        Fact::new('F'),
+        Fact::new('G'),
+        Fact::new('H'),
+        Fact::new('I'),
+        Fact::new('J'),
+        Fact::new('K'),
+        Fact::new('L'),
+        Fact::new('M'),
+        Fact::new('N'),
+        Fact::new('O'),
+        Fact::new('P'),
+        Fact::new('Q'),
+        Fact::new('R'),
+        Fact::new('S'),
+        Fact::new('T'),
+        Fact::new('U'),
+        Fact::new('V'),
+        Fact::new('W'),
+        Fact::new('X'),
+        Fact::new('Y'),
+        Fact::new('Z'),
+    ];
+    let exptok: Vec<Token> = tokenise_str(&expr, &facts);
+    let exptok: Vec<Token> = apply_on_vec(&exptok)?;
+    let rsltok: Vec<Token> = tokenise_str(&rslt, &facts);
+
+    print!("exptok: ");
+    print_tokens(&exptok);
+    print!("rsltok: ");
+    print_tokens(&rsltok);
+
+    assert_eq!(cmp_tokens(exptok, rsltok), ass);
+    Ok(())
 }
 
 #[test]
@@ -51,24 +100,86 @@ fn cmp_tokens(exp: Vec<Token>, res: Vec<Token>) -> bool {
 fn test_ko() {
     assert_eq!(true, false);
 }
-
 #[test]
 fn test_ok() {
     assert_eq!(true, true);
 }
-
 #[test]
-fn test_rpn() -> Result<(), Error> {
+#[should_panic]
+fn test_rpn_ko_op_miss() {
+    let expr: &str = "A + B";
+    let rslt: &str = "A + B";
+    let _ = run_test(expr, rslt, true);
+}
+#[test]
+#[should_panic]
+fn test_rpn_ko_bad_op() {
+    let expr: &str = "A + B";
+    let rslt: &str = "A B ^";
+    let _ = run_test(expr, rslt, true);
+}
+#[test]
+#[should_panic]
+fn test_rpn_ko_letter_a() {
+    let expr: &str = "A + B";
+    let rslt: &str = "A A +";
+    let _ = run_test(expr, rslt, true);
+}
+#[test]
+#[should_panic]
+fn test_rpn_ko_letter_b() {
+    let expr: &str = "A + B";
+    let rslt: &str = "B B +";
+    let _ = run_test(expr, rslt, true);
+}
+#[test]
+fn test_rpn_brackets_01() -> Result<(), Error> {
+    let expr: &str = "A ^ )B + C | D";
+    let rslt: &str = "A B C + ^ D |";
+    let _ = run_test(expr, rslt, false);
+    Ok(())
+}
+#[test]
+fn test_rpn_brackets_02() -> Result<(), Error> {
+    let expr: &str = "(A ^ B + C | D";
+    let rslt: &str = "A B C + ^ D |";
+    let _ = run_test(expr, rslt, false);
+    Ok(())
+}
+#[test]
+fn test_rpn_brackets_03() -> Result<(), Error> {
+    let expr: &str = "A ^ B + C | D)";
+    let rslt: &str = "A B C + ^ D |";
+    let _ = run_test(expr, rslt, false);
+    Ok(())
+}
+#[test]
+fn test_rpn_ok() -> Result<(), Error> {
+    let expr: &str = "A + B";
+    let rslt: &str = "A B +";
+    run_test(expr, rslt, true)
+}
+#[test]
+fn test_rpn_complexe() -> Result<(), Error> {
     let expr: &str = "(C ^ B + A) | ((!E | !B) ^ E)";
     let rslt: &str = "C B A + ^ E! B! | E ^ |";
-
-    let exptok: Vec<Token> = tokenise_str(&expr);
-    let exptok: Vec<Token> = apply_on_vec(&exptok)?;
-    let rsltok: Vec<Token> = tokenise_str(&rslt);
-
-    print_tokens(&exptok);
-    print_tokens(&rsltok);
-
-    assert_eq!(cmp_tokens(exptok, rsltok), true);
-    Ok(())
+    run_test(expr, rslt, true)
+}
+#[test]
+fn test_rpn_00() -> Result<(), Error> {
+    let expr: &str = "A ^ B + C | D";
+    let rslt: &str = "A B C + ^ D |";
+    run_test(expr, rslt, true)
+}
+#[test]
+fn test_rpn_01() -> Result<(), Error> {
+    let expr: &str = "(A ^ B) + (C | D)";
+    let rslt: &str = "A B ^ C D | +";
+    run_test(expr, rslt, true)
+}
+#[test]
+fn test_rpn_02() -> Result<(), Error> {
+    let expr: &str = "A ^ (B + (C | D))";
+    let rslt: &str = "A B C D | + ^";
+    run_test(expr, rslt, true)
 }
