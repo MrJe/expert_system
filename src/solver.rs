c5fa5840127d6b8e3809cfecd5dd8d5307f64f0e
Gaetan POBLON <gpoblon@e1r2p21.42.fr>
2019-10-07 15:47:40 +0200

    wip refacto tree builder

--- src/solver.rs
+++ src/solver.rs
@@ -14,109 +14,89 @@ fn get_plain_queries(queries: Vec<&Fact>) -> Vec<Fact> {
     solved_queries
 }
 
+fn push_operand_rec<'a>(mut graph: Graph<Token<'a>>, token: Token<'a>, cur: &mut NodeIndex) -> Result<Graph<Token<'a>>, Error> {
+    match graph.get(*cur) {
+        Some(mut node) => {
+            if node.lhs.is_none() {
+                *cur = graph.insert_lhs(*cur, token)?;
+            } else if node.rhs.is_none() {
+                *cur = graph.insert_rhs(*cur, token)?;
+            } else {
+                while node.parent.is_some() {
+                    if node.rhs.is_some() {
+                        *cur = node.parent.unwrap();
+                    } else {
+                        *cur = graph.insert_rhs(*cur, token)?;
+                        break;
+                    }
+                    node = graph.get(*cur).unwrap(); // danger
+                }
+            }
+            Ok(graph)
+        },
+        None => {
+            return Err(Error::new(
+                ErrorKind::NotFound,
+                "Tree builder: no current node to push operand",
+            ))
+        }
+    }
+}
+
 // REFACTO WIP TODO
-// fn push_fact_rec<'a>(graph: &'a mut Graph<Token<'a>>, fact: &Fact, mut cur: NodeIndex, rules: &'a Rules, token: Token<'a>, node: &Node<Token<'a>>) -> Result<(), Error> {
-//     if node.lhs.is_none() {
-//         if fact.determined.get() == true {
-//             graph.insert_lhs(cur, token)?;
-//         } else {
-//             let sub_head = graph.insert_lhs(cur, token)?;
-//             checker::infinite_rule_loop(graph, sub_head, fact)?;
-//             generate_tree(graph, fact, sub_head, rules)?;
-//         }
-//     } else {
-//         while node.parent.is_some() {
-//             if node.rhs.is_some() {
-//                 cur = node.parent.unwrap();
-//             } else {
-//                 if fact.determined.get() == true {
-//                     graph.insert_rhs(cur, token)?;
-//                 } else {
-//                     let sub_head = graph.insert_rhs(cur, token)?;
-//                     checker::infinite_rule_loop(graph, sub_head, fact)?;
-//                     generate_tree(graph, fact, sub_head, rules)?;
-//                 }
-//                 break;
-//             }
-//             node = graph.get(cur).unwrap(); // danger
-//         }
-//     }
-//     Ok(())
-// }
+fn push_fact_rec<'a>(mut graph: Graph<Token<'a>>, rules: &'a Rules, token: Token<'a>, fact: &Fact, cur: &mut NodeIndex) -> Result<Graph<Token<'a>>, Error> {
+    match graph.get(*cur) {
+        Some(mut node) => {
+            if node.lhs.is_none() {
+                if fact.determined.get() == true {
+                    graph.insert_lhs(*cur, token)?;
+                } else {
+                    let sub_head = graph.insert_lhs(*cur, token)?;
+                    checker::infinite_rule_loop(&graph, sub_head, fact)?;
+                    graph = generate_tree(graph, fact, sub_head, rules)?;
+                }
+            } else {
+                while node.parent.is_some() {
+                    if node.rhs.is_some() {
+                        *cur = node.parent.unwrap();
+                    } else {
+                        if fact.determined.get() == true {
+                            graph.insert_rhs(*cur, token)?;
+                        } else {
+                            let sub_head = graph.insert_rhs(*cur, token)?;
+                            checker::infinite_rule_loop(&graph, sub_head, fact)?;
+                            graph = generate_tree(graph, fact, sub_head, rules)?;
+                        }
+                        break;
+                    }
+                    node = graph.get(*cur).unwrap(); // danger
+                }
+            }
+            Ok(graph)
+        },
+        None => {
+            return Err(Error::new(
+                ErrorKind::NotFound,
+                "Tree builder: no current node to push fact",
+            ))
+        }
+    }
+}
 
 fn generate_tree<'a>(
-    graph: &mut Graph<Token<'a>>,
+    mut graph: Graph<Token<'a>>,
     queried: &Fact,
     mut cur: NodeIndex,
     rules: &'a Rules,
-) -> Result<(), Error> {
+) -> Result<Graph<Token<'a>>, Error> {
     for rule in rules.iter() {
         if rule.implies_fact(queried) {
             for token in rule.lhs.iter() {
                 let token = *token;
                 if token.is_operand() {
-                    match graph.get(cur) {
-                        Some(mut node) => {
-                            if node.lhs.is_none() {
-                                cur = graph.insert_lhs(cur, token)?;
-                            } else if node.rhs.is_none() {
-                                cur = graph.insert_rhs(cur, token)?;
-                            } else {
-                                while node.parent.is_some() {
-                                    if node.rhs.is_some() {
-                                        cur = node.parent.unwrap();
-                                    } else {
-                                        cur = graph.insert_rhs(cur, token)?;
-                                        break;
-                                    }
-                                    node = graph.get(cur).unwrap(); // danger
-                                }
-                            }
-                        },
-                        None => {
-                            return Err(Error::new(
-                                ErrorKind::NotFound,
-                                "Tree builder: no current node to push operand",
-                            ))
-                        }
-                    }
+                    graph = push_operand_rec(graph, token, &mut cur)?;
                 } else if let Some(fact) = token.fact {
-                    match graph.get(cur) {
-                        // Some(mut node) => push_fact_rec(graph, fact, cur, rules, token, node)?, // node: &Node
-                        Some(mut node) => {
-                            if node.lhs.is_none() {
-                                if fact.determined.get() {
-                                    graph.insert_lhs(cur, token)?;
-                                } else {
-                                    let sub_head = graph.insert_lhs(cur, token)?;
-                                    checker::infinite_rule_loop(graph, sub_head, fact)?;
-                                    generate_tree(graph, fact, sub_head, rules)?;
-                                }
-                            } else {
-                                while node.parent.is_some() {
-                                    if node.rhs.is_some() {
-                                        cur = node.parent.unwrap();
-                                    } else {
-                                        if fact.determined.get() {
-                                            graph.insert_rhs(cur, token)?;
-                                        } else {
-                                            let sub_head = graph.insert_rhs(cur, token)?;
-                                            checker::infinite_rule_loop(graph, sub_head, fact)?;
-                                            generate_tree(graph, fact, sub_head, rules)?;
-                                        }
-                                        break;
-                                    }
-                                    node = graph.get(cur).unwrap(); // danger
-                                }
-                            }
-                        }
-                        None => {
-                            return Err(Error::new(
-                                ErrorKind::NotFound,
-                                "Tree builder: no current node to push fact",
-                            ))
-                        }
-                    }
+                    graph = push_fact_rec(graph, rules, token, fact, &mut cur)?;
                 } else {
                     return Err(Error::new(
                         ErrorKind::InvalidData,
@@ -126,14 +106,14 @@ fn generate_tree<'a>(
             }
         }
     }
-    Ok(())
+    Ok(graph)
 }
 
 pub fn solve(queries: Vec<&Fact>, rules: Rules) -> Result<Vec<Fact>, Error> {
     for fact in queries.iter() {
         let mut graph: Graph<Token> = Graph::new();
         let root: NodeIndex = graph.add_query(Token::new_fact(&fact));
-        generate_tree(&mut graph, fact, root, &rules)?;
+        graph = generate_tree(graph, fact, root, &rules)?;
         // println!("{:#?}", graph);
         print_tree_to_file(&graph);
     }
